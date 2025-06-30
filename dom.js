3087cabba1944d9382221f210f070d2c98aec795
Marri-Yaswanth <marrisiva2408@gmail.com>
2025-06-30 14:03:08 +0530

    Done with Dom

---
+++ dom.js
@@ -0,0 +1,8 @@
+const myDiv = document.getElementById('main-title').innerHTML="Yaswanth";
+console.log(myDiv);
+
+const main1 = document.createElementByClassName('paragraph');
+console.log(main1);
+
+const main2 = document.createElementByTagName('li');
+console.log(main2);
